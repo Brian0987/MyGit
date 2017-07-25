57530e579955e161f9a410d10cb4d4b573130975
Brian0987 <newgjh0000@gmail.com>
2017-07-25 14:46:49 +1200

    Learning jQuery Chapter 02 exercise

---
+++ Learning jQuery/Chapter 02/myscript.js
@@ -0,0 +1,7 @@
+/* My first jQuery code */
+
+// change all paragraphs to red and italic
+$("p").css({"color":"red", "font-style":"italic"});
+
+// change all h1 headings to blue and the text to "Hello"
+$("h1").text("Hello").css("color","blue");
